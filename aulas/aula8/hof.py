9fb71df4c418eda940a9c6a651422b14d1917127
Cauê Lucas <36139948+cauebucci@users.noreply.github.com>
2023-10-16 08:43:53 -0300

    Aulas 08 e 09

---
+++ aulas/aula8/hof.py
@@ -0,0 +1,9 @@
+from pc import dobro, quadrado
+
+def processar(titulo, lista, funcao):
+    print(f'Processando {titulo}')
+    for i in lista:
+        print(i, '==>', funcao(i))
+
+processar('Dobros de 1 a 10', range(1,11), dobro) 
+processar('Quadrados de 1 a 10', range(1,11), quadrado)
---
+++ aulas/aula8/pc.py
@@ -0,0 +1,10 @@
+def dobro(x):
+    return x * 2
+
+def quadrado(x):
+    return x ** 2
+
+a = dobro(10)
+b = dobro(10)
+c = dobro 
+d = c(20) 
